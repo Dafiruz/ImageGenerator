a348e01bec8c3466d4d622ade1c01179cc0ea4a0
Dafiruz <m4gn3t46@gmail.com>
2017-08-15 23:29:01 +0100

    first commit

---
+++ ImageGenerator/Form1.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace ImageGenerator
+{
+    partial class ImageGenerator
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ImageGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.DoubleBuffered = true;
+            this.Name = "ImageGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Generator";
+            this.Load += new System.EventHandler(this.ImageGenerator_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ImageGenerator_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ImageGenerator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageGenerator
+{
+    public partial class ImageGenerator : Form
+    {
+        Graphics paper;
+        private SolidBrush brush;
+        int x = 0, y = 0, swap = 0;
+        int max_height, max_width;
+
+        public ImageGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageGenerator_Load(object sender, EventArgs e)
+        {
+            brush = new SolidBrush(Color.Black);
+            max_height = this.Height;
+            max_width = this.Width;
+        }
+
+        private void ImageGenerator_Paint(object sender, PaintEventArgs e)
+        {
+            
+            paper = e.Graphics;
+            draw(paper);
+        }
+
+        private void draw(Graphics paper)
+        {
+            for (int h = 0; h < max_height; h++)
+			{
+                for (int w = 0; w < max_width; w++)
+			    {
+
+			    }
+			}
+            if (swap == 0)
+            {
+                paper.FillRectangle(brush, x, y, 1, 1);
+                swap = 1;
+            }
+            else
+            {
+                paper.FillRectangle(brush2, x, y, 1, 1);
+                swap = 0;
+            }
+
+        }
+    }
+}
